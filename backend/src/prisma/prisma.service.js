008326c61d3c7c8d65d3a9d0b9af2fda8bec8fc5
Francesco Arrigo <francescoarrigo@Mac.homenet.telecomitalia.it>
2026-03-20 14:42:27 +0100

    fix: configure local runtime for docker and prisma

--- backend/src/prisma/prisma.service.js
+++ backend/src/prisma/prisma.service.js
@@ -1,8 +1,17 @@
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
+  constructor() {
+    super({
+      adapter: new PrismaPg({
+        connectionString: process.env.DATABASE_URL,
+      }),
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
